4afbf012f00ba697797e7536143d50485328e7e7
Jonas Norlinder <github.jonas.norlinder@norlinder.nu>
2015-09-17 19:15:50 +0200

    Some ideas for adding two numbers by command

--- console_interpreter.py
+++ console_interpreter.py
@@ -1,5 +1,28 @@
 #Ask user for input
 expression = raw_input("Please, our dear paying customer enter your awsome command: ")
+print "Raw input :" + expression
 
 if '+' in expression:
-    print "I'm going to add"
+    #B Specifies what to remove from string
+    b = "+"
+
+    expression = expression.replace(b, " ")
+    print "New expression: " + expression
+
+    print int(expression)
+
+    expression = expression.split(' ')
+
+
+'''
+    arr = [int(num) for num in expression]
+
+
+    print "Array :" + expression
+
+    print "expression[0]: " + expression[0]
+    print "expression[1]: " + expression[1]
+    #print "expression[2]: " + expression[2]
+
+    print (expression[0])+(expression[1])
+'''
